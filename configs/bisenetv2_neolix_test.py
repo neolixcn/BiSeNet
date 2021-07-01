3c0888ad081570e20c60afbf40f5590c4e7fb380
Pyten <pan_teng@foxmail.com>
2021-07-01 15:48:08 +0800

    add more configs for different setting

---
+++ configs/bisenetv2_neolix_test.py
@@ -0,0 +1,21 @@
+
+## bisenetv2
+cfg = dict(
+    dataset='neolix',
+    model_type='bisenetv2',
+    class_num=5,
+    num_aux_heads=4,
+    lr_start = 1e-3, # 5e-2,
+    weight_decay=5e-4,
+    warmup_iters = 1000,
+    max_iter = 50000, #150000,
+    im_root='/nfs/neolix_data1/neolix_dataset/test_dataset/freespace_segmentation/neolix_freespace_fisheye',
+    train_im_anns='',
+    val_im_anns='/nfs/neolix_data1/neolix_dataset/test_dataset/freespace_segmentation/neolix_freespace_fisheye/test.txt',
+    scales=[0.25, 2.],
+    cropsize=[512,1280], #[512, 1024],
+    ims_per_gpu= 8,
+    use_fp16=False, #True,
+    use_sync_bn=False,
+    respth="/data/pantengteng/bisenet",#'/nfs/neolix_data1/models/panteng/bisenetv2',
+)
--- configs/bisenetv2_org.py
+++ configs/bisenetv2_org.py
@@ -5,18 +5,17 @@ cfg = dict(
     model_type='bisenetv2',
     class_num= 19,
     num_aux_heads=4,
-    lr_start = 5e-2, # 0.1# 
-    weight_decay= 5e-4,
+    lr_start = 5e-2,
+    weight_decay=5e-4,
     warmup_iters = 1000,
     max_iter = 150000,
     im_root='/nfs/neolix_data1/OpenSource_dataset/freespace_segmentation/cityscapes',
     train_im_anns='/nfs/neolix_data1/OpenSource_dataset/freespace_segmentation/cityscapes/trainImages.txt',
     val_im_anns='/nfs/neolix_data1/OpenSource_dataset/freespace_segmentation/cityscapes/valImages.txt',
     scales=[0.25, 2.],
-    cropsize=[512,1280], # 736,
-    ims_per_gpu=6, #4
-    use_fp16=True, #False
-    use_sync_bn=True, # False
-    respth="/data/pantengteng/bisenet", # '/nfs/neolix_data1/models/panteng/bisenetv2',
+    cropsize=[512, 1280], #512
+    ims_per_gpu=8,
+    use_fp16=True,
+    use_sync_bn=False,
+    respth='/nfs/neolix_data1/models/panteng/bisenetv2',
 )
-
